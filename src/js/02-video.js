8dd9d547df0e6f3e95f37ffa45b4f6a25c518927
Olga Saltanovskaya <saltanoffskaya@gmail.com>
2021-11-02 14:17:15 +0200

    create a video player. when reloading the page, save the playing time

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,29 @@
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+let LOCALSTORAGE_KEY = 'videoplayer-current-time';
+const currentTime = localStorage.getItem(LOCALSTORAGE_KEY);
+
+player.on('timeupdate', throttle(onTimeGet, 1000));
+
+function onTimeGet() {
+  player
+    .getCurrentTime()
+    .then(function (seconds) {
+      localStorage.setItem(LOCALSTORAGE_KEY, seconds);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+
+function setTime() {
+  if (currentTime) {
+    player.setCurrentTime(currentTime).catch(function (error) {
+      console.log(error);
+    });
+    localStorage.removeItem(LOCALSTORAGE_KEY);
+  }
+}
+window.addEventListener('DOMContentLoaded', setTime);
